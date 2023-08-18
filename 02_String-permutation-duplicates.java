deabae5eda313128ca3ac8e1a91a07e3f828105e
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-08-18 23:50:46 +0200

    Update 02_String-permutation-duplicates.java

--- 02_String-permutation-duplicates.java
+++ 02_String-permutation-duplicates.java
@@ -1,1 +1,21 @@
+class Solution {
+  public static void dfs(String str, String, perm, List<String> list) {
+    if (str.length() == 0) {
+      list.add(perm);
+    }
+    Set<Character> set = new HashSet<>();
+    for (int i = 0; i < str.length(); i++) {
+      char ch = str.charAt(i);
+      if ()
+    }
+  }
+  public List<String> find_permutation(String S) {
+    char[] arr = S.toCharArray();
+    Arrays.sort(arr);
+    String str = new String(arr);
 
+    List<String> list =new ArrayList<>();
+    dfs(str, "", list);
+    return list;
+  }
+}

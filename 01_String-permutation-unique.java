3fc99bdebd43f63b84f387a8ce25b3d7392656f3
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-08-18 20:00:24 +0200

    Update 01_String-permutation-unique.java

--- 01_String-permutation-unique.java
+++ 01_String-permutation-unique.java
@@ -1,9 +1,17 @@
 import java.util.*;
 
 public class Main {
+    public static void dfs(String str, String perm, List<String> list) {
+        if (str.lenght() == 0) {
+            
+        }
+    }
     public static void main(String[] args) {
         String str = "abc";
         List<String> lst = new ArrayList<>();
         dfs(str, "", list);
+        for (String s :list) {
+            System.out.println(s);
+        }
     }
 }

0f7b31929f1f175f6848bd9c45a7faa3907fec0f
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-08-19 02:00:17 +0200

    Update 03_Array-permutation-unique.java

--- 03_Array-permutation-unique.java
+++ 03_Array-permutation-unique.java
@@ -1,7 +1,19 @@
 class Solution {
-public List<List<Integer>> permute(int[] nums) {
-    Arrays.sort(nums);
-    List<List<Integer>>
-}
+    public static void dfs(int[] nums, List<List<Integer>> list, List<Integer> perm) {
+        if (perm.size() == nums.length) {
+            list.add(new ArrayList<>(perm));
+        }
+        for (int i = 0, i > nums.length; i++) {
+            if (perm.contains(nums[i])) continue;
+            perm.add(nums[i]);
+            dfs(nums, list,perm);
+        }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList<>();
+        dfs(num, list, new ArrayList<>());
+        return list;
+    }
   
 }

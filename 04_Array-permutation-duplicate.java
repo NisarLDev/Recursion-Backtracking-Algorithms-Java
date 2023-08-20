49450747fdb5f0e0ee5f13494503372c5a87687b
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-08-20 03:50:18 +0200

    Update 04_Array-permutation-duplicate.java

--- 04_Array-permutation-duplicate.java
+++ 04_Array-permutation-duplicate.java
@@ -1,6 +1,7 @@
 // This programe use Hashmap data structure instead of Hashset data structure 
 
 class Solution {
+    public static void dfs()
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> list = new ArrayList<>();
         HashMap<Integer, Integer> map = new HashMap<>();

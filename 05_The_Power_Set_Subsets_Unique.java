2d14b79fed8e40a50ebcb3a7a3dc1a0f7ca2415b
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-03-28 23:52:28 +0100

    Finished

--- 05_The_Power_Set_Subsets_Unique.java
+++ 05_The_Power_Set_Subsets_Unique.java
@@ -1,10 +1,14 @@
 class Solution{
   public static void dfs(int[] nums, List<List<Integer>> list, List<Integer> sub, int start) {
     list.add(new ArrayList<>(sub));
-    
+    for (int i = start; i < nums.length; i++){
+      sub.add(nums[i]);
+      dfs(nums, list, sub, i + 1);
+      sub.remove(sub.size() - 1);
+    }
   }
   public List<List<Integer>> subsets(int[] nums) {
     List<List<Integer>> list = new ArrayList<>();
     
   }
-}
+} // O(n * 2^n)

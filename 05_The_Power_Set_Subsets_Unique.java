58f69082342015ecefbcdf1cfccab6e8e380b54b
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-03-28 23:46:38 +0100

    Update 05_The_Power_Set_Subsets_Unique.java

--- 05_The_Power_Set_Subsets_Unique.java
+++ 05_The_Power_Set_Subsets_Unique.java
@@ -1,4 +1,8 @@
 class Solution{
+  public static void dfs(int[] nums, List<List<Integer>> list, List<Integer> sub, int start) {
+    list.add(new ArrayList<>(sub));
+    
+  }
   public List<List<Integer>> subsets(int[] nums) {
     List<List<Integer>> list = new ArrayList<>();
     
